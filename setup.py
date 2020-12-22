64a187d5c9730bdd1f7096e4b28bbf677f9c6c99
atharva-2001 <aryaa_1@rknec.edu>
2020-12-22 18:13:52 +0530

    version update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 # This call to setup() does all the work
 setup(
     name="toggldash",
-    version="1.0.4",
+    version="1.0.5",
     description="plots data from toggl track",
     long_description=long_description,
     long_description_content_type="text/markdown",
